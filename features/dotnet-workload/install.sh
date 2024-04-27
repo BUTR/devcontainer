8a9ad5e99b8cb7ef331231ba9d58326378434ac9
Vitalii Mikhailov <personal@aragas.org>
2024-04-28 01:02:11 +0300

    Typo

--- features/dotnet-workload/install.sh
+++ features/dotnet-workload/install.sh
@@ -4,6 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo "(*) Installing dotnet workload"
 
 dotnet workload update
-dotnet workload ${WORKLOAD}
+dotnet workload restore ${WORKLOAD}
 
 echo "Done!"
